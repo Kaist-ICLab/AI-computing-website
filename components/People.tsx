e43d47c2bb3cafd5a9c0062f08705a1c7553f739
diptags <rd.pradipta@gmail.com>
2026-02-25 04:40:21 +0900

    Small Adjustments for Faculty page

--- components/People.tsx
+++ components/People.tsx
@@ -48,7 +48,7 @@ const People: React.FC<{ lang: Language; title: string }> = ({
   title,
 }) => {
   return (
-    <section className="py-20 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 bg-white">
+    <section className="pt-8 pb-12 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 bg-white">
       <div className="flex flex-col md:flex-row md:items-end justify-between mb-20 gap-8">
         <div>
           <h2 className="text-4xl sm:text-5xl font-black text-[#004191] tracking-tighter">
@@ -62,7 +62,7 @@ const People: React.FC<{ lang: Language; title: string }> = ({
         </div>
       </div>
 
-      <div className="space-y-24">
+      <div className="space-y-16">
         <div>
           <h3 className="text-xs font-black text-slate-400 mb-10 uppercase tracking-[0.4em] flex items-center gap-4">
             <span className="w-8 h-px bg-slate-200"></span>
@@ -87,9 +87,8 @@ const People: React.FC<{ lang: Language; title: string }> = ({
                   <div className="absolute top-4 right-4 w-2 h-2 bg-[#004191] rounded-full scale-0 group-hover:scale-100 transition-transform"></div>
                 </div>
                 <p
-                  className={`font-black text-slate-900 mb-1 min-h-[28px] ${
-                    lang === "ko" ? "text-lg" : "text-base"
-                  }`}
+                  className={`font-black text-slate-900 mb-1 min-h-[28px] ${lang === "ko" ? "text-lg" : "text-base"
+                    }`}
                 >
                   {lang === "ko" ? member.ko : member.en}
                 </p>
@@ -126,9 +125,8 @@ const People: React.FC<{ lang: Language; title: string }> = ({
                   <div className="absolute top-4 right-4 w-2 h-2 bg-[#004191] rounded-full scale-0 group-hover:scale-100 transition-transform"></div>
                 </div>
                 <p
-                  className={`font-black text-slate-900 mb-1 min-h-[28px] ${
-                    lang === "ko" ? "text-lg" : "text-base"
-                  }`}
+                  className={`font-black text-slate-900 mb-1 min-h-[28px] ${lang === "ko" ? "text-lg" : "text-base"
+                    }`}
                 >
                   {lang === "ko" ? member.ko : member.en}
                 </p>
