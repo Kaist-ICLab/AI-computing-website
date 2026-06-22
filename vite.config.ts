c45b4b999fc13b4ba42d8c031babe2ef6e7c15ba
diptags <rd.pradipta@gmail.com>
2026-06-22 15:06:36 +0900

    Revert build time compile Tailwind CSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(() => {
   return {
@@ -12,7 +11,6 @@ export default defineConfig(() => {
     },
     plugins: [
       react(),
-      tailwindcss(),
     ],
     resolve: {
       alias: {
